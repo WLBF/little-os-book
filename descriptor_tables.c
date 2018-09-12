7c446a4b59c0cf1fa77a3a7669e679971fad6264
wlbf <lbf1353@live.com>
2018-09-12 12:52:15 +0800

    rewrite gdt comments

--- descriptor_tables.c
+++ descriptor_tables.c
@@ -1,45 +1,34 @@
-//
-// descriptor_tables.c - Initialises the GDT and IDT, and defines the 
-// default ISR and IRQ handler.
-// Based on code from Bran's kernel development tutorials.
-// Rewritten for JamesM's kernel development tutorials.
-//
-
 #include "descriptor_tables.h"
 #include "gdt_flush.h"
 
-// Lets us access our ASM functions from our C code.
 extern void gdt_flush(unsigned int);
 
-// Internal function prototypes.
 static void init_gdt();
-static void gdt_set_gate(int, unsigned int, unsigned int, unsigned short, unsigned short);
+static void gdt_set_entry(int, unsigned int, unsigned int, unsigned short, unsigned short);
 
-gdt_entry_t gdt_entries[5];
-gdt_ptr_t   gdt_ptr;
+struct gdt_entry gdt_entries[5];
+struct gdt_ptr   gdt_ptr;
 
-// Initialisation routine - zeroes all the interrupt service routines,
-// initialises the GDT and IDT.
 void init_descriptor_tables()
 {
-   // Initialise the global descriptor table.
    init_gdt();
 }
 
+/* init_gdt() - Initialise the global descriptor table. */
 static void init_gdt()
 {
-   gdt_ptr.limit = (sizeof(gdt_entry_t) * 3) - 1;
+   gdt_ptr.limit = (sizeof(struct gdt_entry) * 3);
    gdt_ptr.base  = (unsigned int)&gdt_entries;
 
-   gdt_set_gate(0, 0, 0, 0, 0);                // Null segment
-   gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // Code segment
-   gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // Data segment
+   gdt_set_entry(0, 0, 0, 0, 0);                /* null segment */
+   gdt_set_entry(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); /* code segment */
+   gdt_set_entry(2, 0, 0xFFFFFFFF, 0x92, 0xCF); /* data segment */
 
    gdt_flush((unsigned int)&gdt_ptr);
 }
 
-// Set the value of one GDT entry.
-static void gdt_set_gate(int num, unsigned int base, unsigned int limit, unsigned short access, unsigned short gran)
+/* gdt_set_entry() - Set the value of one GDT entry. */
+static void gdt_set_entry(int num, unsigned int base, unsigned int limit, unsigned short access, unsigned short gran)
 {
    gdt_entries[num].base_low    = (base & 0xFFFF);
    gdt_entries[num].base_middle = (base >> 16) & 0xFF;
--- descriptor_tables.h
+++ descriptor_tables.h
@@ -1,29 +1,37 @@
 #ifndef INCLUDE_DESCRIPTOR_TABLES_H
 #define INCLUDE_DESCRIPTOR_TABLES_H
 
-// This structure contains the value of one GDT entry.
-// We use the attribute 'packed' to tell GCC not to change
-// any of the alignment in the structure.
-struct gdt_entry_struct
+/**
+ * struct gdt_entry - Contains the value of one GDT entry.
+ * @limit_low:      lower 16 bits of the limit.
+ * @base_low:       lower 16 bits of the base.
+ * @base_middle:    next 8 bits of the base.
+ * @access:         access flags, determine what ring this segment can be used in.
+ * @granularity:    aranularity flag, determines the scaling of the segment limit field
+ * @base_high:      last 8 bits of the base.
+ */
+struct gdt_entry
 {
-   unsigned short limit_low;           // The lower 16 bits of the limit.
-   unsigned short base_low;            // The lower 16 bits of the base.
-   unsigned char  base_middle;         // The next 8 bits of the base.
-   unsigned char  access;              // Access flags, determine what ring this segment can be used in.
+   unsigned short limit_low;
+   unsigned short base_low;
+   unsigned char  base_middle;
+   unsigned char  access;
    unsigned char  granularity;
-   unsigned char  base_high;           // The last 8 bits of the base.
+   unsigned char  base_high;
 } __attribute__((packed));
-typedef struct gdt_entry_struct gdt_entry_t;
 
-struct gdt_ptr_struct
+/**
+ * struct gdt_ptr - Contains the value of the GDTR.
+ * @limit:     16 bits table limit for the GDT.
+ * @base:      base address of the GDT
+ */
+struct gdt_ptr
 {
-   unsigned short limit;               // The upper 16 bits of all selector limits.
-   unsigned int base;                // The address of the first gdt_entry_t struct.
+   unsigned short limit;
+   unsigned int base;
 }
  __attribute__((packed));
-typedef struct gdt_ptr_struct gdt_ptr_t;
 
-// Initialisation function is publicly accessible.
 void init_descriptor_tables();
 
 #endif /* INCLUDE_DESCRIPTOR_TABLES_H */
